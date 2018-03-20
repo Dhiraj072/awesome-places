5ca714edf3cac39608875809f4986cc1afbf545b
Dhiraj <dhirajbhatt072@gmail.com>
2018-03-20 23:53:20 +0800

    SharePlaceScreen can add places now

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { View, ScrollView, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
-import AddPlaceForm from '../../components/AddPlaceForm/AddPlaceForm';
 import ImagePicker from '../../components/ImagePicker/ImagePicker';
 import LocationPicker from '../../components/LocationPicker/LocationPicker';
 import { addPlace } from '../../store/actions/places';
 import Heading1 from '../../components/UI/Heading1/Heading1';
 import MainButton from '../../components/UI/MainButton/MainButton';
+import Input from '../../components/UI/Input';
 
 class SharePlaceScreen extends React.Component {
     constructor(props) {
         super(props);
         this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+    }
+    state = {
+        placeName: '',
     }
     onNavigatorEvent = (event) => {
         if (event.type === 'NavBarButtonPress') {
@@ -23,6 +26,24 @@ class SharePlaceScreen extends React.Component {
         }
     }
 
+    placeNameChangedHandler = (placeName) => {
+        this.setState(() => ({ placeName }));
+    }
+
+    handleAddPlace = () => {
+        if (this.state.placeName.trim() !== '') {
+            const place = {
+                key: Math.random(),
+                name: this.state.placeName,
+                image: {
+                    uri: 'https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb',
+                },
+            };
+            this.props.addPlace(place);
+            this.setState(() => ({ placeName: '' }));
+        }
+    };
+
     render() {
         return (
             <ScrollView>
@@ -30,11 +51,14 @@ class SharePlaceScreen extends React.Component {
                     <Heading1 text="Share a place!" />
                     <ImagePicker />
                     <LocationPicker />
-                    <AddPlaceForm />
-                    <MainButton>Add Place</MainButton>
+                    <Input
+                        placeHolder="Enter place name"
+                        value={this.state.placeName}
+                        onChangeText={this.placeNameChangedHandler}
+                    />
+                    <MainButton onPress={this.handleAddPlace}>Add Place</MainButton>
                 </View>
             </ScrollView>
-
         );
     }
 }
