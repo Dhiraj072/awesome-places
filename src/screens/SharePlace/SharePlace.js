27d505d95dfc49525da8d1c51378b78dc2945d81
Dhiraj <dhirajbhatt072@gmail.com>
2018-04-06 00:49:20 +0800

    Set up validation for add place form

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, ScrollView, StyleSheet } from 'react-native';
+import { View, KeyboardAvoidingView, Keyboard, ScrollView, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import ImagePicker from '../../components/ImagePicker/ImagePicker';
 import LocationPicker from '../../components/LocationPicker/LocationPicker';
@@ -7,6 +7,7 @@ import { addPlace } from '../../store/actions/places';
 import Heading1 from '../../components/UI/Heading1/Heading1';
 import MainButton from '../../components/UI/MainButton/MainButton';
 import Input from '../../components/UI/Input';
+import validate from '../../utility/validation';
 
 class SharePlaceScreen extends React.Component {
     static navigatorStyle = {
@@ -18,7 +19,14 @@ class SharePlaceScreen extends React.Component {
         this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
     }
     state = {
-        placeName: '',
+        placeName: {
+            value: '',
+            valid: false,
+            validationRules: {
+                notEmpty: true,
+            },
+            touched: false,
+        },
     }
     onNavigatorEvent = (event) => {
         if (event.type === 'NavBarButtonPress') {
@@ -31,37 +39,58 @@ class SharePlaceScreen extends React.Component {
     }
 
     placeNameChangedHandler = (placeName) => {
-        this.setState(() => ({ placeName }));
+        this.setState((state) => ({
+            placeName: {
+                ...state.placeName,
+                value: placeName,
+                touched: true,
+                valid: validate(placeName, state.placeName.validationRules),
+            },
+        }));
     }
 
     handleAddPlace = () => {
-        if (this.state.placeName.trim() !== '') {
+        if (this.state.placeName.value.trim() !== '') {
             const place = {
                 key: Math.random(),
-                name: this.state.placeName,
+                name: this.state.placeName.value,
                 image: {
                     uri: 'https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb',
                 },
             };
             this.props.addPlace(place);
-            this.setState(() => ({ placeName: '' }));
+            this.setState((state) => ({
+                placeName: {
+                    ...state.placeName,
+                    value: '',
+                    valid: false,
+                    touched: false,
+                },
+            }));
         }
     };
 
     render() {
         return (
             <ScrollView>
-                <View style={styles.container}>
-                    <Heading1 text="Share a place!" />
-                    <ImagePicker />
-                    <LocationPicker />
-                    <Input
-                        placeHolder="Enter place name"
-                        value={this.state.placeName}
-                        onChangeText={this.placeNameChangedHandler}
-                    />
-                    <MainButton onPress={this.handleAddPlace}>Add Place</MainButton>
-                </View>
+                <KeyboardAvoidingView style={styles.container} behavior="padding">
+                    <View>
+                        <Heading1 text="Share a place!" />
+                        <ImagePicker />
+                        <LocationPicker />
+                        <Input
+                            placeHolder="Enter place name"
+                            value={this.state.placeName.value}
+                            onChangeText={this.placeNameChangedHandler}
+                        />
+                        <MainButton
+                            onPress={this.handleAddPlace}
+                            disabled={!this.state.placeName.valid}
+                        >
+                        Add Place
+                        </MainButton>
+                    </View>
+                </KeyboardAvoidingView>
             </ScrollView>
         );
     }
