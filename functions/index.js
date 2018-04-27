36a9dbc80317ef4d3c17058dd43350a4baefa0d7
Dhiraj <dhirajbhatt072@gmail.com>
2018-04-28 00:54:35 +0800

    Add authentication to firebase cloud function

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions');
 const cors = require('cors')({ origin: true });
 const fs = require('fs');
 const UUID = require('uuid-v4');
+const admin = require('firebase-admin');
 
 const gcconfig = {
     projectId: 'awesome-places-1523022274720',
@@ -9,45 +10,61 @@ const gcconfig = {
 };
 const gcs = require('@google-cloud/storage')(gcconfig);
 
+admin.initializeApp({
+    credential: admin.credential.cert(require('./awesome-places'))
+});
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.storeImage = functions.https.onRequest((request, response) => {
-    console.log('Firebase storing image');
-    cors(request, response, () => {
-        const body = JSON.parse(request.body);
-        fs.writeFileSync(
-            '/tmp/uploaded-image.jpg',
-            body.image,
-            'base64',
-            (error) => {
-                console.log(error);
-                return response.status(500).json({ error: error });
-            });
-        const bucket = gcs.bucket('awesome-places-1523022274720.appspot.com');
-        const id = UUID();
-        bucket.upload('/tmp/uploaded-image.jpg', {
-            uploadType: 'media',
-            destination: '/places/' + id + '.jpg',
-            metadata: {
-                metadata: {
-                    contentType: 'image/jpeg',
-                    firebaseStorageDownloadTokens: id,
-                },
-            },
-        }, (error, file) => {
-            if (!error) {
-                response.status(201).json({
-                    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/' +
-                                bucket.name +
-                                '/o/' +
-                                encodeURIComponent(file.name) +
-                                '?alt=media&token=' +
-                                id,
+    let idToken = request.headers.authorizationToken;
+    admin.auth().verifyIdToken(idToken)
+    .then((token) => {
+        console.log('Firebase storing image');
+        cors(request, response, () => {
+            const body = JSON.parse(request.body);
+            fs.writeFileSync(
+                '/tmp/uploaded-image.jpg',
+                body.image,
+                'base64',
+                (error) => {
+                    console.log(error);
+                    return response.status(500).json({ error: error });
                 });
-            } else {
-                response.status(500).json({ error });
-            }
+            const bucket = gcs.bucket('awesome-places-1523022274720.appspot.com');
+            const id = UUID();
+            bucket.upload('/tmp/uploaded-image.jpg', {
+                uploadType: 'media',
+                destination: '/places/' + id + '.jpg',
+                metadata: {
+                    metadata: {
+                        contentType: 'image/jpeg',
+                        firebaseStorageDownloadTokens: id,
+                    },
+                },
+            }, (error, file) => {
+                if (!error) {
+                    response.status(201).json({
+                        imageUrl: 'https://firebasestorage.googleapis.com/v0/b/' +
+                                    bucket.name +
+                                    '/o/' +
+                                    encodeURIComponent(file.name) +
+                                    '?alt=media&token=' +
+                                    id,
+                    });
+                } else {
+                    response.status(500).json({ error });
+                }
+            });
         });
-    });
+        return response;
+    })
+    .catch((error) => {
+        response.status(403).json({
+            error: 'Unauthorized!',
+        })
+        throw error;
+    })
 });
+
